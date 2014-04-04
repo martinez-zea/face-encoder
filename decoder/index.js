abb6dd071d7d35911441412760a7667d83eef9c6
Gabriel Zea <zea@randomlab.net>
2014-04-03 21:12:56 -0500

    motor ans buttons

--- decoder/index.js
+++ decoder/index.js
@@ -13,7 +13,20 @@ var sensor_lectures = [],
   sensor,
   index = 0,
   previous_read = null,
+  previous_decode = null,
   portrait = []
+
+// hardware
+var motor, front,
+  back, start,
+  MOTOR_PWM = 7,
+  MOTOR_DIRECTION = 6,
+  FORWARD_SPEED = 200, //really slow, it's inverted
+  REVERSE_SPEED = 255,
+  FRONT_PIN = 8,
+  BACK_PIN = 9,
+  START_PIN = 10
+
 
 // main route
 // TODO: express?
@@ -47,6 +60,37 @@ board.on("ready", function() {
 
   //child.exec("open http://localhost:3000/views/index.html")
 
+  // motor controll
+  motor = new five.Motor({
+    pins: { pwm: MOTOR_PWM, dir: MOTOR_DIRECTION}
+  });
+
+  board.repl.inject({
+    motor: motor
+  });
+
+  //end of line
+  front = new five.Button(8);
+  back = new five.Button(9);
+  start = new five.Button(10);
+
+  // logic of buttons
+  start.on("down", function() {
+    console.log("start");
+    motor.forward(FORWARD_SPEED); //buggy
+  });
+
+  front.on("down", function() {
+    console.log("front");
+    motor.reverse(REVERSE_SPEED);
+  });
+
+  back.on("down", function() {
+    console.log("back");
+    motor.stop()
+  });
+
+
   io.sockets.on('connection', function (socket){
     socket.emit('board', { status: 'ready' });
   })
@@ -60,7 +104,7 @@ board.on("ready", function() {
 
     digitalSmooth(this.value, sensor_lectures, function(smooth, raw){
       //trainDecoder(smooth)
-      decode(smooth)
+     // decode(smooth)
       io.sockets.emit('sensor', { raw: raw, smooth: smooth });
     })
 
@@ -71,28 +115,57 @@ board.on("ready", function() {
 function decode (data) {
   var  rounded = Math.round(data)
 
+  //console.log("ro",rounded)
   if (previous_read != rounded) {
 
-    if(rounded >= 320){
+    if(rounded >= 290){
       console.log("empty")
+      //buildPortrait(-1)
     } else if (rounded < 320 && rounded > 245) {
       console.log("5mm") //base
+      //buildPortrait(0)
     } else if (rounded <= 245 && rounded > 230) {
       console.log("10mm") //1
+      //buildPortrait(1)
     } else if (rounded <= 230 && rounded > 220) {
       console.log("15mm") //2
-    } else if (rounded <= 219 && rounded > 180) {
+      //buildPortrait(2)
+    } else if (rounded <= 220 && rounded > 180) {
       console.log("20mm") //3
+      //buildPortrait(3)
     } else if (rounded <= 180 && rounded > 165) {
       console.log("25mm") //4
-    } else if (rounded <= 165 && rounded > 160) {
+      //buildPortrait(4)
+    } else if (rounded <= 165 && rounded > 150) {
       console.log("30mm") //5
+      //buildPortrait(5)
+    } else if (rounded <= 150 ) {
+      console.log("35mm") //5
+      //buildPortrait(6)
     } else {
       console.log("rounded", rounded)
     }
 
     previous_read = rounded
   }
+}
+
+function buildPortrait (measure) {
+  if (previous_decode != measure) {
+    if (measure !== -1 && measure !== 0) {
+      portrait.push(measure)
+     // console.log("mea",measure)
+      // if (measure === 6) {
+      //   portraitDone(portrait)
+      // };
+    }
+    //console.log(portrait)
+    previous_decode = measure
+  };
+}
+
+function portraitDone(data){
+  console.log("portrait",data)
 }
 
 function trainDecoder (data){
