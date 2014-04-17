ed4bb78da917bd117e223baf6efd0fb3a95b8d9b
Gabriel Zea <zea@randomlab.net>
2014-04-17 10:17:41 -0500

    move file

--- encoder/src/picam.js
+++ encoder/src/picam.js
@@ -4,27 +4,22 @@
 // interact with the raspberry camera
 var RaspiCam = require('raspicam')
 
-var Shutter = {
-  camera: null,
-
-  setup: function (filename) {
-    this.camera = new RaspiCam({
-      mode: 'photo',
-      encoding: 'png',
-      output: __dirname+'/static/img/'+filename,
-      timeout: 0,
-      nopreview: true
-    })
-  },
-
-  click: function(){
-    this.camera.start()
-  },
-
-  off: function (){
-    this.camera.stop()
-  },
-
+function Shutter (filename){
+  this.camera = new RaspiCam({
+    mode: 'photo',
+    encoding: 'png',
+    output: __dirname+'/static/img/'+filename,
+    timeout: 0,
+    nopreview: true
+  })
 }
 
-module.exports.shutter = Shutter
+Shutter.prototype.click = function() {
+  this.camera.start()
+}
+
+Shutter.prototype.off = function() {
+  this.camera.stop()
+}
+
+module.exports = Shutter
